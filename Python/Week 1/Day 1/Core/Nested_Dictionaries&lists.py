2184d4eeea8cef5037f14d73244c94673ef76faa
HamzaWeti <watihamza21@gmail.com>
2023-11-15 14:06:18 +0100

    nested

---
+++ Python/Week 1/Day 1/Core/Nested_Dictionaries&lists.py
@@ -0,0 +1,64 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+# 1
+x[1][0] = 15
+print (x)
+# 2
+students[0]['last_name'] = 'Bryant'
+print(students)
+# 3
+sports_directory ["soccer"][0]='Andres'
+print(sports_directory)
+# 4
+z[0]['y'] = 30
+print(z)
+
+# 1 
+def iterateDictionary(some_list):
+    for student in some_list:
+        output_str = ""
+        for key, value in student.items():
+            output_str += f"{key} - {value}, "
+        print(output_str[:-2])  
+
+students = [
+    {'first_name': 'Michael', 'last_name': 'Jordan'},
+    {'first_name': 'John', 'last_name': 'Rosales'},
+    {'first_name': 'Mark', 'last_name': 'Guillen'},
+    {'first_name': 'KB', 'last_name': 'Tonel'}
+]
+
+iterateDictionary(students)
+
+# 3 
+def iterateDictionary2(key_name, some_list):
+    for dictionary in some_list:
+        print(dictionary[key_name])
+
+print("Output for 'first_name':")
+iterateDictionary2('first_name', students)
+
+print("\nOutput for 'last_name':")
+iterateDictionary2('last_name', students)
+# 4
+def printInfo(some_dict):
+    for key, values in some_dict.items():
+        print(f"{len(values)} {key.upper()}")
+        for value in values:
+            print(value)
+        print()
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+printInfo(dojo)
