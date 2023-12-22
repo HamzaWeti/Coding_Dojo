5e194f325c4cb617e060096a1a4b7d36bdf2c81a
HamzaWeti <watihamza21@gmail.com>
2023-12-22 09:15:15 +0100

    first

---
+++ MERNStack/week_1/Day_3/Core/first-jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello Dojo!</h1>
+    <h3>Things I need vto do:</h3>
+    <ol>
+      <li>Learn React</li>
+      <li>Cimb Mt. Everest</li>
+      <li>Run a marathon</li>
+      <li>Feed the dogs</li>
+    </ol>
+    </div>
+  );
+}
+
+export default App;
