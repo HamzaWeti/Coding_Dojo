e441f3acea66f3c3b8ac47c83b65c00c07decd08
HamzaWeti <watihamza21@gmail.com>
2024-01-24 13:56:24 +0100

    Done

--- C Sarp .net/week_1/Day_1/Practice/DebuggingPractice/Program.cs
+++ C Sarp .net/week_1/Day_1/Practice/DebuggingPractice/Program.cs
@@ -1,19 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// Challenge 1
+﻿// Challenge 1
 bool amProgrammer = true;
-int Age = 27;
+double Age = 27.9;
 List<string> Names = new List<string>();
-// Names = "Monica";
 Names.Add("Monica");
 Dictionary<string, string> MyDictionary = new Dictionary<string, string>();
 MyDictionary.Add("Hello", "0");
 MyDictionary.Add("Hi there", "0");
+// This is a tricky one! Hint: look up what a char is in C#
 string MyName = "MyName";
-
 // Challenge 2
-List<int> Numbers = new List<int>() {2,3,6,7,1,5};
-for(int i = Numbers.Count-1; i >= 0; i--)
+List<int> Numbers = new List<int>() {2, 3, 6, 7, 1, 5};
+for (int i = Numbers.Count - 1; i >= 0; i--)
 {
     Console.WriteLine(Numbers[i]);
 }
@@ -24,24 +21,31 @@ foreach(int i in MoreNumbers)
 {
     Console.WriteLine(i);
 }
-
 // Challenge 4
-List<int> EvenMoreNumbers = new List<int> {3,6,9,12,14};
-foreach(int num in EvenMoreNumbers)
+List<int> EvenMoreNumbers = new List<int> {3, 6, 9, 12, 14};
+
+for (int i = 0; i < EvenMoreNumbers.Count; i++)
 {
-    if(num % 3 == 0)
+    if (EvenMoreNumbers[i] % 3 == 0)
     {
-        // num = 0;
+        EvenMoreNumbers[i] = 0;
     }
 }
 
-// Challenge 5
-string MyString = "superduberawesome";
-// MyString[7] = "p";
+// Now EvenMoreNumbers will be modified, and you can print or use the modified list.
+foreach (int num in EvenMoreNumbers)
+{
+    Console.WriteLine(num);
+}
 
+// Challenge 5
+// What can we learn from this error message?
+string MyString = "superduberawesome";
+// MyString[7] = 'p';
 // Challenge 6
+// Hint: some bugs don't come with error messages
 Random rand = new Random();
-int randomNum = rand.Next(12);
+int randomNum = rand.Next(12,12);
 if(randomNum == 12)
 {
     Console.WriteLine("Hello");
