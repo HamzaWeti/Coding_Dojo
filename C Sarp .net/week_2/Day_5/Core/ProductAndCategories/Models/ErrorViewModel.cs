83ff37cfcb14e0903a1e0d79b8e8dffd7ec63240
HamzaWeti <watihamza21@gmail.com>
2024-02-08 13:51:17 +0100

    done

--- C Sarp .net/week_2/Day_5/Core/ProductAndCategories/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ProductAndCategories.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
